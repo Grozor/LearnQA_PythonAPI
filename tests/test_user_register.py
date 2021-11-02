dc86647101eda093e22406b50373c092bb94a1d3
Grozor <grozorka@gmail.com>
2021-11-03 00:12:15 +0300

    Some updates before Ex17

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,8 +1,8 @@
-import requests
 import pytest
 import string
 import random
 
+from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from datetime import datetime
@@ -11,55 +11,29 @@ from datetime import datetime
 class TestUserRegister(BaseCase):
     exclude_fields = ["password", "username", "firstName", "lastName", "email"]
     huge_firstname = ''.join(random.choice(string.ascii_lowercase) for i in range(251))
-
-    def setup(self):
-        base_part = 'learnqa'
-        domain = 'example.com'
-        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
-        self.email = f"{base_part}{random_part}@{domain}"
-        self.email_without_at = f"{base_part}{random_part}{domain}"
-        self.email_without_base_part = f"@{domain}"
-        self.email_without_domain = f"{base_part}{random_part}@"
+    email_without_at = "learnqaexample.com"
 
     def test_create_user_successfully(self):
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': self.email
-        }
+        data = self.prepare_registration_data()
 
-        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+        response = MyRequests.post("/user", data=data)
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
 
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': email
-        }
+        data = self.prepare_registration_data(email)
 
-        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+        response = MyRequests.post("/user", data=data)
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode(
             "utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content '{response.content}'"
 
     def test_create_user_without_at(self):
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': self.email_without_at
-        }
+        data = self.prepare_registration_data(self.email_without_at)
 
-        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+        response = MyRequests.post("/user", data=data)
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Invalid email format"
 
@@ -70,12 +44,12 @@ class TestUserRegister(BaseCase):
             'username': 'learnqa',
             'firstName': 'learnqa',
             'lastName': 'learnqa',
-            'email': self.email
+            'email': 'learnqa@example.com'
         }
 
         del data[exclude_field]
 
-        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+        response = MyRequests.post("/user", data=data)
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"The following required params are missed: {exclude_field}"
 
@@ -85,10 +59,10 @@ class TestUserRegister(BaseCase):
             'username': 'learnqa',
             'firstName': 'l',
             'lastName': 'learnqa',
-            'email': self.email
+            'email': 'learnqa@example.com'
         }
 
-        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+        response = MyRequests.post("/user", data=data)
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"The value of 'firstName' field is too short"
 
@@ -98,9 +72,9 @@ class TestUserRegister(BaseCase):
             'username': 'learnqa',
             'firstName': self.huge_firstname,
             'lastName': 'learnqa',
-            'email': self.email
+            'email': 'learnqa@example.com'
             }
 
-        response = requests.post("https://playground.learnqa.ru/api/user", data=data)
+        response = MyRequests.post("/user", data=data)
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"The value of 'firstName' field is too long"
